b98f5102e1081f771a6ca1a040976de71b6f869a
rehork <67607997+rehork@users.noreply.github.com>
2023-06-20 15:58:39 +0200

    Update prop.mk

--- prop.mk
+++ prop.mk
@@ -7,6 +7,6 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap, net.sourceforge.opencamera \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap,net.sourceforge.opencamera \
 persist.camera.dual.camera=0
 
